5e9cdefe158af18e7b71880292976fad0bff9d25
ViniGarcia10 <viniciusgarcia49@gmail.com>
2020-03-14 08:48:24 -0400

    remove product from cart

--- web/src/store/modules/cart/reducer.js
+++ web/src/store/modules/cart/reducer.js
@@ -18,6 +18,17 @@ export default function cart(state = [], action) {
         }
       });
 
+    case 'REMOVE_DELETE_CART':
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(
+          itemCart => itemCart.id === action.id
+        );
+
+        if (productIndex >= 0) {
+          draft.splice(productIndex, 1);
+        }
+      });
+
     default:
       return state;
   }
